74a779500406be3dc189ae9332dc506916d88b01
Charlie Dunderdale <cjpdee@gmail.com>
2018-11-22 11:44:06 +0000

    Removed modular version of reddit-oop

--- js_data-structure/my-reddit/app.js
+++
@@ -1,8 +0,0 @@
-// import 'js/user_prototype.js';
-import test from './js/user_adminFunctions.js';
-//console.log('something');
-test();
-//import * as thing from '/js/user_adminFunctions';
-// import 'js/dom_objects.js';
-// import 'js/dom_functions.js';
-// import 'js/_global.js';
